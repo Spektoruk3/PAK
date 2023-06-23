a80b6cd94016f26bcae382937f7806ef268367c9
vladimir15l <v.leshin@g.nsu.ru>
2023-06-23 14:16:35 +0700

    add tests

---
+++ src/test/test_program.py
@@ -0,0 +1,18 @@
+import unittest
+
+class TestFluidCube(unittest.TestCase):
+
+    def testFluid(self):
+        self.assertEqual(1, 1)
+        return True
+
+
+
+
+
+
+def run_tests():
+     unittest.main()
+
+if __name__ == '__main__':
+    run_tests()
