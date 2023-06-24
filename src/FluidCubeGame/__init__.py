1bf2f01590c63b276d905c9fd94d20f10b1cc1ef
Spektoruk3 <102353513+Spektoruk3@users.noreply.github.com>
2023-06-24 21:11:43 +0700

    Update __init__.py

--- src/FluidCubeGame/__init__.py
+++ src/FluidCubeGame/__init__.py
@@ -1,5 +1,5 @@
-from .FluidCube import FluidCube as FluidCube
-from .main import *
+from . import FluidCube
+from . import main
 
 __author__ = 'Leshin Vladimir, Spektoruk Ilya, Krukovskiy Vasiliy'
 __email__ = 'i.spektoruk@g.nsu.ru'
